bac03ab6cdd6575b839020feae749b49b4992ee2
rafasf13 <62573501+rafasf13@users.noreply.github.com>
2024-09-03 21:29:36 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -10,17 +10,20 @@ function nextSequence() {
   iterator = 0;
   var randomNumber = Math.floor(Math.random() * 4);
   gamePattern.push(buttonColours[randomNumber]);
-  flash(buttonColours[randomNumber]);
-  sound(buttonColours[randomNumber]);
+  repeatPattern();
   console.log(gamePattern);
 }
 
+function play(color) {
+  sound(color);
+  flash(color);
+}
 function sound(color) {
-  var soundFile = new Audio("./sounds/" + color + ".mp3");
+  var soundFile = new Audio("./sounds/" + color + ".m4a");
   soundFile.play();
 }
 function flash(color) {
-  $("." + color).css("background-color", "white");
+  $("." + color).css("background-color", "gray");
   setTimeout(function () {
     $("." + color).css("background-color", color);
   }, 200);
@@ -28,14 +31,23 @@ function flash(color) {
 }
 
 function testSequence() {
+  if (gamePattern.length === 0) {
+    startGame();
+    return;
+  }
   if (gamePattern[iterator] === this.id) {
     flash(this.id);
     sound(this.id);
     iterator++;
-    if (iterator == gamePattern.length) setTimeout(nextSequence, 500);
+    if (iterator == gamePattern.length) setTimeout(nextSequence, 600);
   } else {
-    $("body").addClass("game-over");
     sound("wrong");
+    $("h1").text("Loser! Reiniciar?");
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+
     gamePattern = [];
   }
   console.log("iterator: " + iterator + ", clicked: " + this.id);
@@ -45,5 +57,15 @@ function startGame() {
   if (gamePattern.length === 0) {
     $("body").removeClass("game-over");
     nextSequence();
+  }
+}
+
+function repeatPattern(index = 0) {
+  if (index < gamePattern.length) {
+    setTimeout(function () {
+      flash(gamePattern[index]);
+      sound(gamePattern[index]);
+      repeatPattern(index + 1);
+    }, 500);
   }
 }
